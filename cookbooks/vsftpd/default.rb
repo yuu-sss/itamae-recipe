9eb50ef2afef3df8e7f4ec6d89d7a9a6174dd5fb
yuki <y-sato@mba001.local>
2016-10-21 17:22:17 +0900

    add cookbooks/vsftpd/

--- cookbooks/vsftpd/default.rb
+++ cookbooks/vsftpd/default.rb
@@ -1,7 +1,27 @@
 # Install vsftpd
-package "vsftpd"
 
-# Config file
+remote_file "/etc/vsftpd/vsftpd.conf" do
+  action :nothing
+  owner "root"
+  group "root"
+  mode "600"
+  source "templates/etc/vsftpd/vsftpd.conf.erb"
+end
+
+remote_file "/etc/vsftpd/chroot_list" do
+  action :nothing
+  owner "root"
+  group "root"
+  mode "600"
+  source "templates/etc/vsftpd/chroot_list.erb"
+end
+
+package "vsftpd" do
+  action :install
+  not_if "rpm -q vsftpd"
+  notifies :create, 'remote_file[/etc/vsftpd/vsftpd.conf]'
+  notifies :create, 'remote_file[/etc/vsftpd/chroot_list]'
+end
 
 service "vsftpd" do
   action [:enable, :start]
