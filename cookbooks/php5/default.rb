8d1159e03210035ce612412970be5cd63bc071b8
yuki <y-sato@mba001.local>
2016-10-21 17:00:46 +0900

    fix php5 /etc/php.ini

--- cookbooks/php5/default.rb
+++ cookbooks/php5/default.rb
@@ -4,8 +4,21 @@ package "epel-release" do
   not_if "rpm -q epel-release"
 end
 
+remote_file "/etc/php.ini" do
+  action :nothing
+  owner "root"
+  group "root"
+  mode "644"
+  source "templates/etc/php.ini.erb"
+end
+
+package "php" do
+  action :install
+  not_if "rpm -q php"
+  notifies :create, 'remote_file[/etc/php.ini]'
+end
+
 %w{
-  php
   php-gd
   php-pear
   php-mbstring
