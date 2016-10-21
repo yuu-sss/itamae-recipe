8db1584b9d93a87b841c839a4ed96cb4e312692f
yuki <y-sato@mba001.local>
2016-10-21 16:51:26 +0900

    fix mysql /etc/my.cnf

--- cookbooks/mysql/default.rb
+++ cookbooks/mysql/default.rb
@@ -1,5 +1,18 @@
 # Install MySQL Community Server
+remote_file "/etc/my.cnf" do
+  action :nothing
+  owner "root"
+  group "root"
+  mode "644"
+  source "templates/etc/my.cnf.erb"
+end
+
 package "mysql-server" do
   action :install
   not_if "rpm -q mysql-server"
+  notifies :create, 'remote_file[/etc/my.cnf]'
+end
+
+service "mysqld" do
+  action [:enable, :start]
 end
